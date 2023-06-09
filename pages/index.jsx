ddfb763c8af38840bcc4b46eb5a0428610b0e10a
AryanK1511 <aryankhurana1511@gmail.com>
2023-06-09 16:32:21 -0400

    Added netlify link

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@
 *  Name: Aryan Khurana
 *  Student ID: 145282216
 *  Date: 3 June 2023
-*  Cyclic Link: https://velvety-pothos-a60070.netlify.app
+*  Cyclic Link: https://web422-next-app.netlify.app
 *
 ********************************************************************************/ 
 
